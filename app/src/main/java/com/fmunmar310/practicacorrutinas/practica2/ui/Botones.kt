fa125131a98dadd9faadc1499ab9c87a86327e49
GaelCobain <fmunmar310@g.educaand.es>
2024-01-26 18:24:01 +0100

    parte2 con LiveData

--- app/src/main/java/com/fmunmar310/practicacorrutinas/practica2/ui/Botones.kt
+++ app/src/main/java/com/fmunmar310/practicacorrutinas/practica2/ui/Botones.kt
@@ -8,21 +8,19 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 @Composable
 fun Botones(botonesViewModel: BotonesViewModel){
-    var colorFondo by rememberSaveable {mutableStateOf(1)}
-    var vecesPulsado by rememberSaveable { mutableStateOf(0) }
+    val colorFondo: Int by botonesViewModel.colour.observeAsState(initial = 1)
+    val result: String by botonesViewModel.resultState.observeAsState(initial = "Respuesta de la API ()")
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center){
         Button(
-            onClick = {colorFondo = botonesViewModel.cambiarColor(colorFondo)},
+            onClick = {botonesViewModel.cambiarColor()},
             modifier = Modifier.wrapContentSize(),
             colors = ButtonDefaults.buttonColors(
                 containerColor = if(colorFondo == 1) Color.Red
@@ -30,17 +28,15 @@ fun Botones(botonesViewModel: BotonesViewModel){
         ){
             Text(text = "Cambiar Color")
         }
-        Text(text = "Respuesta de la Api ($vecesPulsado)")
+        Text(text = result)
         Button(
-            onClick = {botonesViewModel.bloqueoApp()
-                      vecesPulsado++},
+            onClick = {botonesViewModel.fetchData() },
             modifier = Modifier.wrapContentSize(),
             colors = ButtonDefaults.buttonColors(
                 containerColor = Color.Blue)
         ){
             Text(text = "Llamar Api")
         }
-
     }
 }
 
--- app/src/main/java/com/fmunmar310/practicacorrutinas/practica2/ui/BotonesViewModel.kt
+++ app/src/main/java/com/fmunmar310/practicacorrutinas/practica2/ui/BotonesViewModel.kt
@@ -1,22 +1,43 @@
 package com.fmunmar310.practicacorrutinas.practica2.ui
 
 import android.app.Application
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class BotonesViewModel (application: Application): AndroidViewModel(application){
+
+    private val _colour = MutableLiveData<Int>(1)
+    val colour: LiveData<Int> = _colour
+    private var _callCount = 0
+    private val _resultState = MutableLiveData<String>()
+    val resultState: LiveData<String> = _resultState
     fun bloqueoApp(){
        Thread.sleep(5000)
     }
-
-    fun cambiarColor(value: Int):Int{
-        val newValue: Int
-        if(value ==1){
-            newValue = value+1
-        }else{
-            newValue = value-1
+    fun fetchData() {
+        _callCount= _callCount.plus(1)
+        //Nos permite crear una corrutina desde un ViewModel
+        viewModelScope.launch {
+            val result = withContext(Dispatchers.IO) {
+                delay(5000)
+                "Respuesta de la API ($_callCount)"
+            }
+            _resultState.value = result
         }
-        return newValue
+    }
+
+    fun cambiarColor() {
+        if(_colour.value!! ==1){
+            _colour.value = _colour.value!! + 1
+        }else{
+            _colour.value = _colour.value!! - 1
+        }
     }
 }
